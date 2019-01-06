b3efe90e3fe980204a617c2eae4746e91c789c16
Pablo Coronel <pablomanuelcoronel@gmail.com>
2019-01-06 18:41:54 -0300

    verbos de formateo, agregado salto de linea

--- Verbos de formateo/formateo.go
+++ Verbos de formateo/formateo.go
@@ -6,11 +6,18 @@ import (
 
 func main() {
 	fmt.Printf("String %s", "Pablo")
+	fmt.Println()
 	fmt.Printf("Float %.2f", 5.3555555)
+	fmt.Println()
 	fmt.Printf("Integer %d", 10)
+	fmt.Println()
 	fmt.Printf("Bool %t", true)
+	fmt.Println()
 	fmt.Printf("Automatico %v", 35.1212)
+	fmt.Println()
 	fmt.Printf("Como en el codigo Go %#v", "\n")
+	fmt.Println()
 	fmt.Printf("TypeOF %T", true)
+	fmt.Println()
 	fmt.Printf("Escapar porcentaje %%")
 }
