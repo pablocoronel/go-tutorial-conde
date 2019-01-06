13f70a55e4628d7d990e72ec13b03aacdc1309dd
Pablo Coronel <pablomanuelcoronel@gmail.com>
2019-01-05 23:33:13 -0300

    verbos de formateo

---
+++ Verbos de formateo/formateo.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("String %s", "Pablo")
+	fmt.Printf("Float %.2f", 5.3555555)
+	fmt.Printf("Integer %d", 10)
+	fmt.Printf("Bool %t", true)
+	fmt.Printf("Automatico %v", 35.1212)
+	fmt.Printf("Como en el codigo Go %#v", "\n")
+	fmt.Printf("TypeOF %T", true)
+	fmt.Printf("Escapar porcentaje %%")
+}
